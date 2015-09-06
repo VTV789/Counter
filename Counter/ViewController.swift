ec7d79bd657d5560ceb182ea7c14dabdf31a5b51
Vinh Thanh Vu <vtv@vinh.com>
2015-09-06 03:37:32 -0400

    First commit
    import UIKit
    
    class ViewController: UIViewController {
    
        override func viewDidLoad() {
            super.viewDidLoad()
            var label = UILabel()
            label.frame = CGRectMake(150,150,60,60)
            label.text = "0"
    
            self.view.addSubview(label)
        }
    }

---
+++ Counter/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Counter
+//
+//  Created by Vinh Vu on 9/6/15.
+//  Copyright (c) 2015 bhag. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var label = UILabel()
+        label.frame = CGRectMake(150,150,60,60)
+        label.text = "0"
+        
+        self.view.addSubview(label)
+    }
+}
+
